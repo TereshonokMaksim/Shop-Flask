314abf8dc66153ecb6d24b8406e705c97ddcb883
TereshonokMaksim2010 <121506340+TereshonokMaksim@users.noreply.github.com>
2024-06-28 10:22:24 +0300

    bot fix

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import threading
 from bot.tele_bot import bot
 
 # Если правда, то будет запущено с помощью потоков
-bot_on = True
+bot_on = False
 
 if __name__ == "__main__":
     if not bot_on:
@@ -11,6 +11,7 @@ if __name__ == "__main__":
 
     else:
         # Thread part (shop website - debug off due to error that occures if using threads)
+        # PLEASE, DO NOT USE ON HOST
 
         thread_website = threading.Thread(target = project.project.run)
         thread_bot = threading.Thread(target = bot.infinity_polling)
